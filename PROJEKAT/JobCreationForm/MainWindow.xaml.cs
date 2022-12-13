3b53267b4b27c7575a6845760b7f2484de151106
DESKTOP-SV2RG4H\Тепица <acivon@outlook.com>
2022-12-13 10:33:58 +0100

    Added gui components without connecting

---
+++ PROJEKAT/JobCreationForm/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JobCreationForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        public SubWindow()
+        {
+            InitializeComponent();
+        }
+
+        //NEED TO SET PRIORITY, START TIME, FINISH TIME, MAX EXECUTION TIME, NAME, NUM_ITERATIONS, SLEEP_TIME
+    }
+}
